693ee4b32f2283c50e200918bcc582f53b736880
Anil Madhavapeddy <anil@recoil.org>
2015-02-25 10:42:00 +0000

    and also correct the ocaml version installed by travis

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -4,7 +4,7 @@ OPAM_VERSION=1.2.0
 case "$OCAML_VERSION,$OPAM_VERSION" in
 4.00.1,1.2.0) ppa=avsm/ocaml40+opam12 ;;
 4.01.0,1.2.0) ppa=avsm/ocaml41+opam12 ;;
-4.02.1,1.2.0) ppa=avsm/ocaml41+opam12 ;;
+4.02.1,1.2.0) ppa=avsm/ocaml42+opam12 ;;
 *) echo Unknown $OCAML_VERSION,$OPAM_VERSION; exit 1 ;;
 esac
 
